4e8c84fce36febe50dc2bec89799841ddcd8704d
Slava Kostenko <shegan782@gmail.com>
2020-08-11 18:24:55 +0300

    new tabs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
-// $(document).ready(function() {
-//     $('.carousel__inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="img/left-arrow.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="img/right-arrow.png"></button>',
-//         responsive: [{
-//             breakpoint: 992,
-//             settings: {
-//                 dots: true,
-//                 arrows: false
-//             }
-//         }]
-//     });
+(function($) {
+    $(function() {
 
-// });
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+                .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+                .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+
+    });
+
+
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog__first').eq(i).toggleClass('catalog__first_active');
+                $('.catalog__second').eq(i).toggleClass('catalog__second_active')
+            })
+        });
+    }
+    toggleSlide('.catalog__link');
+    toggleSlide('.catalog__back');
+})(jQuery);
 
 const slider = tns({
     container: '.carousel__inner',
