fe4e63282dfb066764c825c3548abf0e8b170753
Slava Kostenko <shegan782@gmail.com>
2020-08-16 19:33:18 +0300

    mail form

--- js/main.js
+++ js/main.js
@@ -69,6 +69,22 @@
     valideForm('#order form');
     valideForm('#consultation form');
     $('input[name=phone]').mask("+7 (999) 999-9999");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 })(jQuery);
 
 const slider = tns({
